7e60ea81e685c14056df0d74f455e2aaa3ea4679
David Brown <dmlb2000@gmail.com>
2017-02-21 20:50:54 -0800

    make the policy server actually start

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM python:2-onbuild
 EXPOSE 8181
-CMD [ "python", "./PolicyServer.py" ]
+CMD [ "python", "./ProxyServer.py" ]
--- ProxyServer.py
+++ ProxyServer.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 from json import dumps
 import cherrypy
+from proxy.root import Root
 
 
 def error_page_default(**kwargs):
@@ -18,8 +19,13 @@ def error_page_default(**kwargs):
 
 def main():
     """Main method for running the server."""
-    print('to be written')
+    cherrypy.quickstart(Root(), '/', 'server.conf')
 
 
+cherrypy.config.update({'error_page.default': error_page_default})
+# pylint doesn't realize that application is actually a callable
+# pylint: disable=invalid-name
+application = cherrypy.Application(Root(), '/', 'server.conf')
+# pylint: enable=invalid-name
 if __name__ == '__main__':
     main()

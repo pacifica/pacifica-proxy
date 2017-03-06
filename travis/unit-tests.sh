352c0859560274b1f3e6d5a8ee167469a855a260
David Brown <dmlb2000@gmail.com>
2017-03-05 22:25:09 -0800

    fix #1 add readme and templates (#2)

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -18,4 +18,6 @@ docker-compose stop proxyserver
 export PYTHONPATH=$PWD
 coverage run --include='proxy/*' -m pytest -v
 coverage report -m --fail-under=100
-codeclimate-test-reporter
+if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
+  codeclimate-test-reporter
+fi

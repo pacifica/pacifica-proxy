f625b51267813103f6dfb5528bc80609b7b72451
David Brown <dmlb2000@gmail.com>
2018-05-03 09:14:35 -0700

    fix nginx startup and configuration

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -4,7 +4,7 @@ psql -c 'create database pacifica_metadata;' -U postgres
 if [ "$RUN_LINTS" = "true" ]; then
   pip install pre-commit
 else
-  sed -e "s|@@ROOT_DIR@@|$PWD|g" < travis/nginx.conf.in > travis/nginx.conf
+  sed -e "s|@@ROOT_DIR@@|$PWD|g;s|@@CONF_DIR@@|/etc/nginx|g" < travis/nginx.conf.in > travis/nginx.conf
   nginx -c $PWD/travis/nginx.conf &
   echo $! > nginx.pid
 fi

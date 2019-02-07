43dff8d81836ef9250f989302d8b1f8779ce7516
David Brown <dmlb2000@gmail.com>
2019-02-07 09:13:48 -0800

    Add docker container testing
    This adds docker container testing to travis so it doesn't get messed
    up again
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir ~/.pacifica-proxy
+touch ~/.pacifica-proxy/config.ini
+cp /usr/src/app/server.conf ~/.pacifica-proxy/cpconfig.ini
 uwsgi \
   --http-socket 0.0.0.0:8180 \
   --master \

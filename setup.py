c18db4eeb544cd1cd915ced9cca5923dd31b46c1
David Brown <dmlb2000@gmail.com>
2020-05-16 20:27:06 -0700

    Remove Requirements File (#27)
    This removes the requirements file and moves them to setup.py and
    removes associated references. This also add docs testing and
    updates for pipelines.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@
 # -*- coding: utf-8 -*-
 """Setup and install the proxy."""
 from os import path
-try:  # pip version 9
-    from pip.req import parse_requirements
-except ImportError:
-    from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
 setup(
     name='pacifica-proxy',
@@ -30,5 +24,9 @@ setup(
             'pacifica-proxy=pacifica.proxy.__main__:main'
         ],
     },
-    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+    install_requires=[
+        'cherrypy',
+        'pacifica-namespace',
+        'requests'
+    ]
 )
